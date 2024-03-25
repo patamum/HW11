05a8d1514697e02889a258fecfa9638e436e0064
Svetlana Jourkova <belkina_s@mail.ru>
2024-03-25 23:28:04 +0300

    Добавьте файлы проекта.

---
+++ Task11_1/Linear.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11_1
+{
+    struct Linear
+    {
+        double k;
+        double b;
+        public Linear(double k, double b)
+        {
+            this.k = k;
+            this.b = b;                
+        }
+        public string Root()
+        {
+            if (k == 0 && b != 0)
+                return "Решений нет";
+            if (k == 0 && b == 0)
+                return "Бесконечно много решений";
+            double r1 = -b / k;
+            return $"Решение: {r1}";
+        }
+    }
+}
---
+++ Task11_1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task11_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Уравнение: 0 = k*x + b");
+            Console.WriteLine("Введите коэффициент k");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите коэффициент b");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Linear linear = new Linear(k, b);
+            Console.WriteLine(linear.Root());
+
+
+        }
+    }
+}
